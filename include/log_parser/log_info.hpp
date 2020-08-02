15674b4d7bb5abbd3160d58fc7155f335f025f4e
lijiancong <18221336269@163.com>
2020-08-02 21:56:34 +0800

    更改文件名称

---
+++ include/log_parser/log_info.hpp
@@ -0,0 +1,64 @@
+﻿/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+
+/// @file   log_info.hpp
+/// @brief  用于保存日志信息的数据结构
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2020-08-02 20:50:25
+
+#ifndef INCLUDE_LOG_PARSER_LOG_INFO_HPP_
+#define INCLUDE_LOG_PARSER_LOG_INFO_HPP_
+
+#include <string>
+
+#include "log_parser/log_level.hpp"
+#include "log_parser/log_location.hpp"
+#include "log_parser/log_text.hpp"
+#include "log_parser/log_time.hpp"
+
+namespace lee {
+inline namespace log_detail {
+/// @name     log_info
+/// @brief    用于保存解析后的单条log
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2020-08-01 17:39:55
+/// @warning  线程不安全
+class log_info {
+ public:
+  /// [2020-07-22 20:43:36.931] [trace] trace 15 <In Function: main, File:
+  /// main.cc, Line: 20, PID: 15808>
+  log_info(const std::string& str) {
+    auto begin = str.find_first_of('[');
+    auto end = str.find_first_of(']');
+    log_time time(str.substr(begin, end - begin));
+    time_ = time;
+
+    begin = str.find_first_of('[', begin);
+    end = str.find_first_of(']', begin);
+    log_level level(str.substr(begin, end - begin));
+    level_ = level;
+
+    end = str.find_first_of('<', begin);
+    log_text text(str.substr(begin, end - begin));
+    text_ = text;
+
+    begin = end + 1;
+    log_location location(str.substr(begin));
+    location_ = location;
+  }
+
+ private:
+  log_time time_;
+  log_level level_;
+  log_text text_;
+  log_location location_;
+};
+
+}  // namespace log_detail
+}  // namespace lee
+
+#endif  // INCLUDE_LOG_PARSER_LOG_INFO_HPP_
