cc1de60f77ecda6dc53c178ebeb90df3150fea11
lijiancong <18221336269@163.com>
2020-08-02 20:34:23 +0800

    添加简单log_location模块的测试

---
+++ test/log_location_unittest.cc
@@ -0,0 +1,15 @@
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+
+#include "log_parser/log_location.hpp"
+#include <catch2/catch.hpp>
+
+TEST_CASE("log_location simply test", "[my_log][log_location]"){
+ lee::log_location location("<In Function: main, File: main.cpp, Line: 20, PID: 15808>");
+ REQUIRE(location.get_file_name() == "main.cpp");
+ REQUIRE(location.get_function_name() == "main");
+ REQUIRE(location.get_file_line() == 20);
+ REQUIRE(location.get_thread_id() == 15808);
+}
