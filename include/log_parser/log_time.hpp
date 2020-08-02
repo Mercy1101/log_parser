dd388b26c45aed4d42317bf06d9fbb7c9e4426b9
lijiancong <18221336269@163.com>
2020-08-02 21:57:36 +0800

    添加默认构造函数

--- include/log_parser/log_time.hpp
+++ include/log_parser/log_time.hpp
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+﻿/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
 ///
 /// Use of this source code is governed by a MIT license
 /// that can be found in the License file.
@@ -13,11 +13,10 @@
 #define INCLUDE_LOG_PARSER_LOG_TIME_HPP_
 
 #include <cassert>
-#include <ctime>
+#include <time.h>
 #include <exception>
 #include <string>
 #include <vector>
-
 
 namespace lee {
 inline namespace log_detail {
@@ -29,9 +28,13 @@ inline namespace log_detail {
 /// @warning  线程不安全
 class log_time {
  public:
-  log_time(const std::string& s) {
+  log_time(std::string s) {
+    if (s.empty()) {
+      return;
+    }
+    s = s.substr(1, s.size() - 2);
     auto vec = set(s);
-    if (s.size() != 7) {
+    if (vec.size() != 7) {
       assert(false && "error param in log_time");
     }
     year_ = vec.at(0);
@@ -45,25 +48,37 @@ class log_time {
         get_specific_timestamp(year_, mouth_, day_, hour_, min_, sec_);
     time_stamp_millisec_ = time_stamp_ * 1000 + millisec_;
   }
+  log_time() : log_time("") {}
 
   /// 2020-08-02 15:40:35.783 => {2020, 8, 2, 15, 40, 35, 783}
   std::vector<int> set(const std::string& s) {
     auto date = s.substr(0, s.find_first_of(' '));
     auto date_vec = get_date(date);
 
-    auto time = s.substr(s.find_first_of(' ') + 1, s.find_first_of('.'));
+    auto begin = s.find_first_of(' ') + 1;
+    auto end = s.find_first_of('.');
+    auto time = s.substr(begin, end - begin);
     auto time_vec = get_time(time);
 
-    auto millisec_t = get_millisec(s.substr(s.find_first_of('.') + 1));
+    auto milli_sec_t = get_millisec(s.substr(s.find_first_of('.') + 1));
 
     std::vector<int> res;
     res.reserve(7);
-    res.insert(date_vec.begin(), date_vec.end(), res.end());
-    res.insert(time_vec.begin(), time_vec.end(), res.end());
-    res.push_back(millisec_t);
+    res.insert(res.end(), date_vec.begin(), date_vec.end());
+    res.insert(res.end(), time_vec.begin(), time_vec.end());
+    res.push_back(milli_sec_t);
 
     return res;
   }
+  int get_year() { return year_; }
+  int get_mouth() { return mouth_; }
+  int get_day() { return day_; }
+  int get_hour() { return hour_; }
+  int get_min() { return min_; }
+  int get_sec() { return sec_; }
+  int get_millisec() { return millisec_; }
+  time_t get_time_stamp() { return time_stamp_; }
+  time_t get_time_stamp_millisec() { return time_stamp_millisec_; }
 
  private:
   int year_ = 0;
@@ -78,17 +93,17 @@ class log_time {
 
   std::vector<int> get_date(const std::string& date) {
     size_t begin = 0;
-    auto end = date.find_first_of('_');
+    auto end = date.find_first_of('-');
     int year = std::stoul(date.substr(begin, end));
     begin = end + 1;
-    auto end = date.find_first_of(begin, '_');
+    end = date.find_first_of('-', begin);
     int mouth = std::stoul(date.substr(begin, end));
     begin = end + 1;
     int day = std::stoul(date.substr(begin));
     if (confirm_date_valid(year, mouth, day)) {
       return std::vector<int>{year, mouth, day};
     } else {
-      throw ("invalid date param");
+      throw("invalid date param");
     }
   }
 
@@ -97,7 +112,7 @@ class log_time {
     auto end = time.find_first_of(':');
     auto hour = std::stoi(time.substr(begin, end));
     begin = end + 1;
-    end = time.find_first_of(begin, ':');
+    end = time.find_first_of(':', begin);
     auto min = std::stoi(time.substr(begin, end));
     begin = end + 1;
     end = std::string::npos;
@@ -105,7 +120,7 @@ class log_time {
     if (confirm_time_valid(hour, min, sec)) {
       return std::vector<int>{hour, min, sec};
     } else {
-      throw ("invalid time param");
+      throw("invalid time param");
     }
   }
   int get_millisec(const std::string& millisec) {
@@ -113,7 +128,7 @@ class log_time {
     if (confirm_milli_valid(res)) {
       return res;
     } else {
-      throw ("invalid millisec");
+      throw("invalid millisec");
     }
   }
 
