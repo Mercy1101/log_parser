dc1393ad538130e5ea12244077b34b93e7d70581
lijiancong <18221336269@163.com>
2020-08-02 21:58:10 +0800

    添加空构造函数的单元测试

--- test/log_location_unittest.cc
+++ test/log_location_unittest.cc
@@ -4,12 +4,23 @@
 /// that can be found in the License file.
 
 #include "log_parser/log_location.hpp"
+
 #include <catch2/catch.hpp>
 
-TEST_CASE("log_location simply test", "[my_log][log_location]"){
- lee::log_location location("<In Function: main, File: main.cpp, Line: 20, PID: 15808>");
- REQUIRE(location.get_file_name() == "main.cpp");
- REQUIRE(location.get_function_name() == "main");
- REQUIRE(location.get_file_line() == 20);
- REQUIRE(location.get_thread_id() == 15808);
+
+TEST_CASE("log_location construct test", "[my_log][log_location]") {
+  lee::log_location location(
+      "<In Function: main, File: main.cpp, Line: 20, PID: 15808>");
+  REQUIRE(location.get_file_name() == "main.cpp");
+  REQUIRE(location.get_function_name() == "main");
+  REQUIRE(location.get_file_line() == 20);
+  REQUIRE(location.get_thread_id() == 15808);
+}
+
+TEST_CASE("log_location empty construct test", "[my_log][log_location]") {
+  lee::log_location location;
+  REQUIRE(location.get_file_name() == "");
+  REQUIRE(location.get_function_name() == "");
+  REQUIRE(location.get_file_line() == 0);
+  REQUIRE(location.get_thread_id() == 0);
 }
