7a75440afdf5dd332af739124c12ceaf650c9222
lijiancong <18221336269@163.com>
2020-08-07 21:28:08 +0800

    完成搜索字符串的读取，接下来需要解析成搜索字符串和正则表达式

--- include/log_parser/condition_parser.hpp
+++ include/log_parser/condition_parser.hpp
@@ -22,23 +22,36 @@ inline namespace condition_detail {
 class condition_parser {
  public:
   /// +error, -info, *[regex], +\+, -\-, +\*,
-  static cond_vec parser(const std::string& raw_condition) {
+  ///"+error", "-info", "*[regex]", "+\\+",
+  static cond_vec parser(std::string raw_condition) {
+    raw_condition = replace_backslash(raw_condition);
     raw_condition = strip_space(raw_condition);
-    raw_condition = splice_by_string(raw_condition, ",");
+    std::vector<std::string> vec;
+    vec = splice_by_string(raw_condition, ",");
   }
 
  private:
-  static std::string strip_space(const string& raw_condition) {
+  void replace_backslash(string& raw_condition) {
+    /// TODO(lijiancong): 目前还不确定获取到的字符串读取时能否读取到两个反斜杠
+  }
+  static void strip_space(string& raw_condition) {
     auto begin = raw_condition.find_first_not_of(' ');
     auto end = raw_condition.find_last_not_of(' ');
-    return raw_condition.substr(begin, end);
+    raw_condition = raw_condition.substr(begin, end);
   }
 
-  static std::string split(const string& raw_condition,
-                           const std::string& token = ",") {
-                             
-                           }
-};
+  static std::vector<std::string> split(const string& raw_condition,
+                                        const std::string& token = ",") {
+    using size = std::string::size_type;
+    std::vector<std::string> vec;
+    size begin = raw_condition.find_first_not_of(token, 0);
+    size end = raw_condition.find_first_of(token, begin);
+    while (std::string::npos != begin && std::string::npos != end) {
+      vec.push_back(raw_condition.substr(begin, end - begin));
+      end = raw_condition.find_first_not_of(token, begin);
+      begin = raw_condition.find_first_of(token, end);
+    }
+  };
 }  // namespace condition_detail
-}  // namespace lee
+}  // namespace condition_detail
 #endif  // INCLUDE_CONDITION_PARSER_HPP_
